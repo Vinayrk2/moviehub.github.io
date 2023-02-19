153407160b9f35d8c9d4f30eacedf5674357c6ee
VINAY-KOSHTI <83126054+Vinayrk2@users.noreply.github.com>
2023-02-19 14:17:07 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,1 +1,35 @@
+// $(window).on("load",function(){
 
+// });
+
+$(document).ready(function(){
+	$("#preloader").animate({
+		"opacity":0
+	},1000,"swing",function(){
+		$("#preloader").css({"display":"none"})
+	});
+
+	$("#btnContainer a,#scrollDown a").on("click",function(){
+	var hash;
+
+		if (this.hash !== "") {
+			hash = this.hash;
+			event.preventDefault();
+
+			$("html,body").animate({
+				"scrollTop" : $(hash).offset().top 
+			},"slow")
+		}
+	});
+
+	$("#scrollDiv a").click(function(){
+		if (this.hash != "") {
+			var hash = this.hash;
+			event.preventDefault();
+
+			$("html,body").animate({
+				"scrollTop" : $(hash).offset().top
+			},"slow")
+		}
+	});
+});
